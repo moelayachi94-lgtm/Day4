a7e657836c9e155b3cecd63657a9ed9f0198c88d
Mohammed El Aayachi <moelayachi94@gmail.com>
2026-09-14 14:30:09 +0100

    mm

--- day04/exercices/exercice-14.js
+++ day04/exercices/exercice-14.js
@@ -15,3 +15,9 @@
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
 // TODO: écris ta solution ici.
+let tableau = [1, 2, 3]
+tableau.unshift(tableau.pop())
+
+console.log(tableau);
+
+
